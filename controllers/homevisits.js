cdc0255ccf6aaa866e3ce22936f0d44307ced2ed
bongomin <bongomindaniel@gmail.com>
2019-12-13 15:44:55 +0300

    feat(Resticting Login Till Approved By admin):
    - restricting registered user till aproved by super user

--- controllers/homevisits.js
+++ controllers/homevisits.js
@@ -10,60 +10,32 @@ exports.homeVisits = (req, res) => {
 
 // adding home visit records
 exports.PostFeedBacks = (req, res) => {
-   const errors = [];
-   if (!req.body.scholarship_no) {
-      errors.push({ text: "Please Fill In The Scholarship Number to continue " });
+   const homeVisit = {
+      sponsorship_no: req.body.sponsorship_no,
+      district_local_gov: req.body.district_local_gov,
+      visit_date: req.body.visit_date,
+      house_hold_head: req.body.house_hold_head,
+      village: req.body.village,
+      parish: req.body.parish,
+      division_sub_county: req.body.division_sub_county,
+      district: req.body.district,
+      reached: req.body.reached,
+      yes_date: req.body.yes_date,
+      actions_to_be_taken: req.body.actions_to_be_taken,
+      summarized_reasons: req.body.summarized_reasons,
+      findings_actions: req.body.findings_actions,
+      home_visitor_title: req.body.home_visitor_title,
+      home_vistor: req.body.home_vistor
    }
-   if (errors.length > 0) {
-      res.render('homevisit', {
-         errors: errors,
-         sponsorship_no: req.body.sponsorship_no,
-         district_local_gov: req.body.district_local_gov,
-         visit_date: req.body.visit_date,
-         house_hold_head: req.body.house_hold_head,
-         village: req.body.village,
-         parish: req.body.parish,
-         division_sub_county: req.body.division_sub_county,
-         district: req.body.district,
-         reached: req.body.reached,
-         yes_date: req.body.yes_date,
-         actions_to_be_taken: req.body.actions_to_be_taken,
-         summarized_reasons: req.body.summarized_reasons,
-         findings_actions: req.body.findings_actions,
-         home_visitor_title: req.body.home_visitor_title,
-         home_vistor: req.body.home_vistor
-
+   new HomeVisit(homeVisit).save()
+      .then(homevisit => {
+         res.json(homeVisit)
       })
-   } else {
-      const homeVisit = {
-         sponsorship_no: req.body.sponsorship_no,
-         district_local_gov: req.body.district_local_gov,
-         visit_date: req.body.visit_date,
-         house_hold_head: req.body.house_hold_head,
-         village: req.body.village,
-         parish: req.body.parish,
-         division_sub_county: req.body.division_sub_county,
-         district: req.body.district,
-         reached: req.body.reached,
-         yes_date: req.body.yes_date,
-         actions_to_be_taken: req.body.actions_to_be_taken,
-         summarized_reasons: req.body.summarized_reasons,
-         findings_actions: req.body.findings_actions,
-         home_visitor_title: req.body.home_visitor_title,
-         home_vistor: req.body.home_vistor
-      }
-
-      new HomeVisit(homeVisit).save()
-         .then(homevisit => {
-            res.json(homevisit)
-         })
-         .catch(err => {
-            return err;
-         })
-
-   }
-
-
-
+      .catch(err => {
+         return err;
+      })
 
 }
+
+
+
