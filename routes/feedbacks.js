4f4a97c21d16bb79d35bc78bd8f6b94b1ee55dff
MAC <mac@Danny-MacBook-Pro.local>
2019-12-15 15:27:24 +0300

    feat(implimenting auto fetching of data from database):

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var { feedBackPage, postFeedBack, allFeedBacks } = require('../controllers/feedbacks');
+var { feedBackPage, postFeedBack, allFeedBacks, fetchStudentDetailsForFeedback } = require('../controllers/feedbacks');
 
 router.get('/', feedBackPage);
 router.get('/feedbacks', allFeedBacks);
 router.post('/send', postFeedBack);
+router.post('/requestStudentDetails', fetchStudentDetailsForFeedback)
 
 module.exports = router;
 
