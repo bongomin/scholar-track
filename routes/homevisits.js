321fdb9f5f921245a49c86c1303d767f8eb268a1
bongomin <candanobongomindaniel@gmail.com>
2019-12-30 11:58:24 +0300

    feat(Main page front Interface):
    - added the front end interface navigations

--- routes/homevisits.js
+++ routes/homevisits.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var { homeVisits, PostFeedBacks } = require('../controllers/homevisits')
+var { homeVisits, PostFeedBacks, fetching_home_visit_data } = require('../controllers/homevisits')
 
 // homeVisits page endpoint
 router.get('/', homeVisits);
 router.post('/add', PostFeedBacks);
+router.post('/fetch_home_visits', fetching_home_visit_data)
 
 
 module.exports = router;
